0c2cc2f00cf0695e1893be780006a54f53942835
Stefan Penner <stefan.penner@gmail.com>
2016-01-14 19:04:10 -0800

    fix tagName typo

--- app/components/uptime-day.js
+++ app/components/uptime-day.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 const { computed } = Ember;
 
 export default Ember.Component.extend({
-  tagName: 'uptime-days',
+  tagName: 'uptime-day',
   color: computed('day.up', function() {
     return this.get('day.up') ? '#8cc665' : '#ccc';
   }),
